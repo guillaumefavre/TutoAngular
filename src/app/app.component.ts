f0d6802631bfa0b7e734909a5044d2447332fafb
gfavre <guillaumefavre56@gmail.com>
2017-06-20 21:43:36 +0200

    Edition d'un artiste via input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+export class Artist {
+  id: number;
+  name: string;
+  speciality: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{artist.name}} is a {{artist.speciality}} hero !</h2>
+  <div><label>id: </label>{{artist.id}}</div>
+  <div>
+  	<label>Name: </label>
+  	<input [(ngModel)]="artist.name" placeholder="name">
+  </div>
+  <div>
+  	<label>Speciality: </label>
+  	<input [(ngModel)]="artist.speciality" placeholder="speciality">
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'Mon appli';
+  title = 'Rock\'n Roll Hall of Fame';
+  artist: Artist = {
+  	id: 1,
+  	name: 'Angus Young',
+  	speciality: 'guitar'
+  };
 }
