580c53ffa126acbc74ce52b34010b092b25c8485
FAVRE <guillaume.favre@capgemini.com>
2017-06-21 14:11:48 +0200

    affichage d'une liste d'artistes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,29 +6,28 @@ export class Artist {
   speciality: string;
 }
 
+const ARTISTS: Artist[] = [
+  { id: 1, name: 'Keyth Richards', speciality: 'guitar' },
+  { id: 2, name: 'Angus Young', speciality: 'guitar'  },
+  { id: 3, name: 'Slash', speciality: 'guitar'  },
+  { id: 4, name: 'Mick Jagger', speciality: 'singing'  },
+  { id: 5, name: 'Roger Waters', speciality: 'bass'  }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   template: `
   <h1>{{title}}</h1>
-  <h2>{{artist.name}} is a {{artist.speciality}} hero !</h2>
-  <div><label>id: </label>{{artist.id}}</div>
-  <div>
-  	<label>Name: </label>
-  	<input [(ngModel)]="artist.name" placeholder="name">
-  </div>
-  <div>
-  	<label>Speciality: </label>
-  	<input [(ngModel)]="artist.speciality" placeholder="speciality">
-  </div>
+  <h2>My Artists</h2>
+  <ul class="artists">
+    <li *ngFor="let artist of artists">
+    <span class="badge">{{artist.id}}</span> {{artist.name}} is a {{artist.speciality}} hero
+  </ul>
   `
 })
 export class AppComponent {
   title = 'Rock\'n Roll Hall of Fame';
-  artist: Artist = {
-  	id: 1,
-  	name: 'Angus Young',
-  	speciality: 'guitar'
-  };
+  artists = ARTISTS;
 }
