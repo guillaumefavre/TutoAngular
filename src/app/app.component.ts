1ae0d94f34ef35d35a4ef35e59720310be9997d0
gfavre <guillaumefavre56@gmail.com>
2017-06-21 20:15:26 +0200

    style liste artistes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,56 @@ const ARTISTS: Artist[] = [
     <li *ngFor="let artist of artists">
     <span class="badge">{{artist.id}}</span> {{artist.name}} is a {{artist.speciality}} hero
   </ul>
-  `
+  `,
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .artists {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 25em;
+  }
+  .artists li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .artists li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .artists li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .artists .text {
+    position: relative;
+    top: -3px;
+  }
+  .artists .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
 export class AppComponent {
   title = 'Rock\'n Roll Hall of Fame';
