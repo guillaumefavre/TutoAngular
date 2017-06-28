541420f496ad4106e461614cf14ac3e3f2f9a633
gfavre <guillaumefavre56@gmail.com>
2017-06-28 20:13:34 +0200

    ajout vue tableau de bord + redirection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,32 @@ import { AppComponent } from './app.component';
 import { ArtistDetailComponent } from './artist-detail.component'
 import { ArtistsComponent } from './artists.component';
 import { ArtistService } from './artist.service';
+import { DashboardComponent } from './dashboard.component'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ArtistDetailComponent,
-    ArtistsComponent
+    ArtistsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
     RouterModule.forRoot([ // Router is composed by an array of route definitions (two parts : path and component)
     {
+	  path: '',
+	  redirectTo: '/dashboard',
+	  pathMatch: 'full'
+	},{
       path: 'artists',
       component: ArtistsComponent // Component that the router create when navigating to this route
-    }
+    },
+    {
+	  path: 'dashboard',
+	  component: DashboardComponent
+	}
   ])
   ],
   providers: [ArtistService],
